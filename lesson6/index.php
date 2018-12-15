7810f294c04a488d6e97a3fbdb2c86cb7bbd00af
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2018-12-15 16:27:45 +0300

    Update index.php

--- lesson6/index.php
+++ lesson6/index.php
@@ -1,27 +1,60 @@
 <!--Создать форму-калькулятор с операциями: сложение, вычитание, умножение, деление. 
 Не забыть обработать деление на ноль! Выбор операции можно осуществлять с помощью тега <select>.-->
 <?
+    if(isset($_POST)){
+        $sign = $_POST[run];
+    } else{
+        $sign = "";
+    }
+    $x = $_POST[x];
+    $y = $_POST[y];
 
+
+    switch($_POST[run]){
+        case "+":
+            $z = $x + $y;
+            break;
+        case "*":
+            $z = $x * $y;
+            break;
+        case "-":
+            $z = $x - $y;
+            break;
+        case "/":
+            if($y != 0){
+                $z = $x / $y;    
+            } else {
+                $z = "Error";    
+            }
+            break;
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Gallery</title>
+    <title>Calc</title>
+    <style>
+        input[type="text"]{
+            width: 35px;
+            height: 20px;
+        }
+    </style>
 </head>
 <body>
     <form action="" method="POST">
-        <input name="leftElem" type="text" value="<?=$_POST[leftElem]?>">
-        <select name="doing">
-            <option value="addition"> + </option>
-            <option value="multiplication"> * </option>
-            <option value="subtraction"> - </option>
-            <option value="division"> / </option>
+        <input name="x" type="text" value="<?=$x?>">
+        <select name="run">
+            <option> <?=$sign?> </option>
+            <option value="+"> + </option>
+            <option value="*"> * </option>
+            <option value="-"> - </option>
+            <option value="/"> / </option>
         </select>
-        <input name="rightElem" type="text" value="<?=$_POST[rightElem]?>">
+        <input name="y" type="text" value="<?=$y?>">    
         <input name="send" type="submit" value=" = ">
-        <input name="result" type="text" value="">    
+        <input name="z" type="text" value="<?=$z?>">    
     </form>
 </body>
 </html>
